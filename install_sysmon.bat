4fe3274033fa740576524c82374750c64e3f55dd
Eric Capuano <capuanoe@gmail.com>
2018-02-21 10:54:23 -0600

    Update install_sysmon.bat

--- install_sysmon.bat
+++ install_sysmon.bat
@@ -18,7 +18,7 @@ echo [+] Downloading Sysmon...
 @powershell (new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon64.exe','C:\ProgramData\sysmon\sysmon64.exe')"
 echo [+] Downloading Sysmon config...
 @powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ReconInfoSec/70-sysmon/master/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ReconInfoSec/70-sysmon/master/Auto_update.bat','C:\ProgramData\sysmon\Auto_Update.bat')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ReconInfoSec/70-sysmon/master/auto_update.bat','C:\ProgramData\sysmon\auto_update.bat')"
 sysmon64.exe -accepteula -i sysmonconfig-export.xml
 sc failure Sysmon actions= restart/10000/restart/10000// reset= 120
 echo [+] Sysmon Successfully Installed!
